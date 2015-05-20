b3b97e85c5d4ba3135d9d397a72b70663d5b62e8
fatboyzyc <fatboyzyc@gmail.com>
2015-05-20 19:24:09 +0800

    let's rock

---
+++ README.md
@@ -0,0 +1,22 @@
+从虾米下载音乐吧
+=================
+
+## 注意
+- 只能下载试听的低品质音乐和对应歌词
+- 艺术家、专辑封面等等东西都被忽略了
+- 这是个 F# 脚本
+
+## How to use it
+
+### 找到专辑 id
+- 进入虾米播放器
+- 用 chrome 开发者工具指向左边的某个专辑找到类似：
+	<div class="collect-item" data-id="********">
+  data-id 的值就是专辑 id
+
+### Windows
+- 需要 .net framework 3.5+
+- 需要 F# (vs2010+ or Visual Studio 2010 F# 2.0 Runtime SP1)
+- 找到你的专辑 id, 新建一个 input.txt （与 xiamidown.fsx 同目录）并输入 id, 一行一个
+- 右键 xiamidown.fsx 选 Run with F# Interactive...
+- 下载开始 等待 。。。 下载的 .mp3 与 .lrc 出现在脚本同一目录下
---
+++ xiamidown.fsx
@@ -0,0 +1,99 @@
+﻿#r "System.Xml.dll"
+#r "System.Xml.Linq.dll"
+
+open System
+open System.Threading
+open System.IO
+open System.Net
+open System.Xml.Linq
+
+let INPUT_FILE = "input.txt"
+let XIAMI_PLAYLIST_URI = "http://www.xiami.com/song/playlist/id/{0}/type/3"
+let DOWNFILE_PARALLEL_LIMIT = 5
+
+let limitedParallel (n : int) (tseq : Async<'a> seq) =
+    let ts = Seq.toArray tseq
+    let rets = Array.zeroCreate ts.Length
+    let count = ref -1
+    let rec worker wid =
+        async {
+            let c = Interlocked.Increment count
+            if c < ts.Length then
+                let! ret = ts.[c]
+                rets.[c] <- ret
+                do! worker wid
+        }
+    async {
+        let ws = Array.init n worker
+        do! ws |> Async.Parallel |> Async.Ignore
+        return rets
+    }
+
+let decodeLocation(location : String) =
+    let n = Convert.ToInt32(location.[0]) - Convert.ToInt32('0')
+    let l = location.Length - 1
+    let d, m = l / n, l % n
+    seq {
+        for i in [| 0 .. d |] do
+            for j in [| 0 .. n - 1 |] do
+                yield location.[d * j + Math.Min(j, m) + i + 1] 
+    }
+    |> Seq.take(l)
+    |> Seq.toArray
+    |> fun cs -> new String(cs)
+    |> Uri.UnescapeDataString
+    |> fun s -> s.Replace('^', '0')
+
+let downFile(uri : Uri, file : string) =
+    async {
+        if File.Exists(file) then 
+            Console.Error.WriteLine("File '{0}' already exist, download canceled.", file)
+        else
+            let req = WebRequest.Create(uri)
+            let! resp = req.AsyncGetResponse()
+            use stream = resp.GetResponseStream()
+            stream.CopyTo(File.OpenWrite(file))
+            Console.Error.WriteLine("File '{0}' downloaded.", file)
+    }
+
+let downTrack(track : XElement) =
+    seq {
+        let ns = track.GetDefaultNamespace()
+        let title = track.Element(ns.GetName "title").Value
+
+        let location = track.Element(ns.GetName "location").Value
+        yield downFile(new Uri(decodeLocation location), title + ".mp3")
+
+        let lyric = track.Element(ns.GetName "lyric").Value
+        if lyric <> "" then
+            yield downFile(new Uri(lyric), title + ".lrc")
+    }
+
+let downPlaylist (stream : Stream) =
+    let d = XDocument.Load stream
+    let ns = d.Root.GetDefaultNamespace()
+    d.Root.Element(ns.GetName "trackList").Elements(ns.GetName "track")
+    |> Seq.map downTrack
+    |> Seq.concat
+    |> limitedParallel DOWNFILE_PARALLEL_LIMIT
+    |> Async.Ignore
+
+let job (uri : Uri) =
+    async {
+        Console.Error.WriteLine("Playlist '{0}'", uri.ToString())
+        let req = WebRequest.Create(uri)
+        let! resp = req.AsyncGetResponse()
+        use stream = resp.GetResponseStream()
+        do! downPlaylist stream
+    }
+
+let ids = File.ReadAllLines(INPUT_FILE)
+
+for id in ids do
+    if id <> "" then
+        use writer = new StringWriter()
+        writer.Write(XIAMI_PLAYLIST_URI, id)
+        let uri = new Uri (writer.ToString())
+        job uri |> Async.RunSynchronously
+
+Console.Error.WriteLine("Finish!!")
